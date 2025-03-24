19ff988f556af0cdff781e8e1abb79dfd7c50fe5
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-25 03:24:51 +0530

    Unit Test for Accounts Table and Random util

---
+++ util/random.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+// Create a new random source
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func RandomInt(min, max int64) int64 {
+	return min + seededRand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[seededRand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[seededRand.Intn(n)]
+}
